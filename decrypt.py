89125350509fe50f3d1d090c90f3acf2010db5c0
avdhruvast <dhruva@iiitkalyani.ac.in>
2018-03-14 02:36:53 +0530

    initial commit

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,9 @@
 from Crypto.Cipher import AES
-def decrypt(ciphertext):
-	
-	obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+def decrypt(ciphertext , user_id):
+	extra_append = 16 - len(user_id)
+	for i in range (0,extra_append):
+		user_id = user_id + '?' 
+	obj = AES.new( user_id , AES.MODE_CBC, user_id )
 	output_message = obj.decrypt(ciphertext)
 	output_message = output_message.replace('?','')
 	return output_message
--- encrypt.py
+++ encrypt.py
@@ -1,13 +1,18 @@
 from Crypto.Cipher import AES
-def encrypt(input_message):
+def encrypt(input_message , user_id):
+	input_message = input_message + user_id 
 	input_len = len(input_message)
 	if input_len % 16 != 0:
 		extra_append = 16 - (input_len % 16)
 		for i in range (0,extra_append):
 			input_message = input_message + '?' 
-	obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+	extra_append = 16 - len(user_id)
+	for i in range (0,extra_append):
+		user_id = user_id + '?'
+	obj = AES.new( user_id , AES.MODE_CBC, user_id)
 	ciphertext = obj.encrypt(input_message)
 	return ciphertext
+
 
 
 
